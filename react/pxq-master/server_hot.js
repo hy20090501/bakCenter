cbf5b2e78fe37dbd456dea36aa0182a013d3d7d3
huangyang <hy20090501@163.com>
2017-03-04 17:41:31 +0800

    react ...

---
+++ react/pxq-master/server_hot.js
@@ -0,0 +1,30 @@
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.config.hot');
+
+//代理服务器
+var proxy = [{
+	path: '/*/*', //必须得有一个文件地址，如果顶层文件夹名字不同，则用/*代替
+	target: 'http://shopro.putaoevent.com',
+	host: 'shopro.putaoevent.com',
+	secure: false
+}];
+var server = new WebpackDevServer(webpack(config), {
+	publicPath: config.output.publicPath,
+	hot: true,
+	historyApiFallback: true,
+	inline: true,
+	progress: true,
+	stats: {
+	     colors: true
+	},
+	proxy
+});
+
+//将其他路由，全部返回index.html
+server.app.get('*', function(req, res) {
+	res.sendFile(__dirname + '/index.html')
+});
+server.listen(8080, function() {
+	console.log('正常打开8080端口')
+});
